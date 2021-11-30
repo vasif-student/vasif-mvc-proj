07106f34c3a6b74d3b9124d575c57af884a518b7
vasif-student <vasifah@code.edu.az>
2021-11-30 17:07:11 +0400

    vasif-mvc

---
+++ mvc-proj/Controllers/AboutController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvc_proj.Controllers
+{
+    public class AboutController
+    {
+        public string Index()
+        {
+            return "Bizim Haqqimizda";
+        }
+    }
+}
